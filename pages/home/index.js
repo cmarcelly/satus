4712229c83385c2610a4e19692bef36f2916e68d
GFier <guido@studiofreight.com>
2023-05-24 13:57:34 +0100

    simplify branch

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,32 +1,10 @@
-import { Image } from '@studio-freight/compono'
-import cn from 'clsx'
 import { Layout } from 'layouts/default'
 import Shopify from 'lib/shopify'
-import s from './home.module.scss'
 
-export default function Home({ productsArray }) {
+export default function Home() {
   return (
     <Layout>
-      <section className={s.hero}>
-        <h1 className={s.title}>Shopify Starter</h1>
-      </section>
-      <section>
-        <div className={cn('layout-grid', s['product-grid'])}>
-          {productsArray.map((product, key) => (
-            <a key={`product-${key}`} href={`/${product.slug}`}>
-              <div className={s.image}>
-                <Image
-                  src={product.images[0].src}
-                  alt=""
-                  layout="fill"
-                  priority
-                />
-              </div>
-              <p className={s['product-name']}>{product.name}</p>
-            </a>
-          ))}
-        </div>
-      </section>
+      <h1>satus</h1>
     </Layout>
   )
 }
@@ -37,12 +15,7 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      productsArray: [
-        ...productsArray,
-        ...productsArray,
-        ...productsArray,
-        ...productsArray,
-      ],
+      productsArray: productsArray,
     },
     revalidate: 1,
   }
