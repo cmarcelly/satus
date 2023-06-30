ad104ee3735c9c171ecf3edb7694a99769f9a41e
Clément Roche <rchclement@gmail.com>
2023-06-30 14:42:45 +0200

    sass variables

--- layouts/default/index.js
+++ layouts/default/index.js
@@ -1,18 +1,11 @@
 import { Cursor, CustomHead, Scrollbar } from '@studio-freight/compono'
-import { useDebug } from '@studio-freight/hamo'
 import { Lenis, useLenis } from '@studio-freight/react-lenis'
 import cn from 'clsx'
 import { Footer } from 'components/footer'
 import { Header } from 'components/header'
-import dynamic from 'next/dynamic'
 import Router from 'next/router'
 import { useEffect } from 'react'
 import s from './layout.module.scss'
-
-const Orchestra = dynamic(
-  () => import('libs/orchestra').then(({ Orchestra }) => Orchestra),
-  { ssr: false }
-)
 
 export function Layout({
   seo = { title: '', description: '', image: '', keywords: '' },
@@ -21,8 +14,6 @@ export function Layout({
   className,
   layout,
 }) {
-  const debug = useDebug()
-
   const lenis = useLenis()
 
   useEffect(() => {
@@ -50,11 +41,6 @@ export function Layout({
           <Footer data={layout.footers.footer.content} />
         </div>
       </Lenis>
-      {debug && (
-        <>
-          <Orchestra />
-        </>
-      )}
     </>
   )
 }
